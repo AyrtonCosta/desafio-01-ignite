1ac6a67c9ca59dcce852c057b3269ec03b001639
ayrton da costa e silva alves <ayrtondsp30@gmail.com>
2021-03-15 15:32:43 -0300

    todo finalizado

--- desafio-01-ignite/src/components/TaskList.tsx
+++ desafio-01-ignite/src/components/TaskList.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 import '../styles/tasklist.scss'
 
@@ -18,7 +18,7 @@ export function TaskList() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     if(!newTaskTitle) return;
 
-    const newTask ={
+    const newTask = {
       id:Math.random(),
       title:newTaskTitle,
       isComplete:false
@@ -26,23 +26,17 @@ export function TaskList() {
 
     setTasks(oldState => [...oldState, newTask]);
     setNewTaskTitle('');
+   
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
-    const newTask = tasks.map(task => task.id === id ? { 
-      ...task,
-      isComplete: !task.isComplete
-    }: task);
-
-    setTasks(newTask);
-
+    
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
-    const filteredTask = tasks.filter(task => task.id !== id);
-    setTasks(filteredTask);
+    
   }
 
   return (
